7305d6bbc95922c2e6d6087fc6dbd91ae00f1762
shixianqing <1434501995@qq.com>
2018-09-19 22:18:50 +0800

    Signed-off-by: shixianqing <1434501995@qq.com> 修改lua文件

--- 字符串.lua
+++ 字符串.lua
@@ -16,3 +16,27 @@ str2 = [[
 ]];
 
 print("字符串3是：",str2 );
+
+
+--[[
+string.reverse(arg)
+字符串反转
+--]]
+
+local a = "abcd";
+local b = string.reverse(a);
+print('a---->' .. a .. '   b---->' .. b);
+
+--[[
+string.format(...)
+返回一个类似printf的格式化字符串
+--]]
+
+print(string.format("this is value : %s\n",a));
+
+--[[
+	string.len(arg)
+计算字符串长度。
+--]]
+
+print('字符串的长度为：------>  ' .. string.len(a));
